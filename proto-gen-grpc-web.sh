b82680ff7215c611e89c22dccf52352e3544e51a
Phill <phillipsngs@gmail.com>
2023-12-02 16:07:54 -0800

    sketchy proto setup for frontend

--- proto-gen-grpc-web.sh
+++ proto-gen-grpc-web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p ./frontend/src/proto
-protoc --proto_path=. app/src/main/proto/raft.proto \
+protoc --proto_path=. app/src/main/proto/trade.proto \
   --plugin=protoc-gen-grpc-web=./node_modules/.bin/protoc-gen-grpc-web \
   --js_out=import_style=commonjs:./frontend/src/proto \
   --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./frontend/src/proto \
